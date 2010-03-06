5bb1d8a9b858decbf9acfbe9f07554f1c17ce559
Jonas Haag <jonas@lophus.org>
2010-03-06 19:41:35 +0100

    examples/lastfmsubmit: Little error workaround

--- examples/lastfmsubmit.py
+++ examples/lastfmsubmit.py
@@ -7,14 +7,18 @@ CLIENT = LastFmClient('python-moc-example')
 
 @mocevent.register('song-changed')
 def submit_to_lastfm(song_info):
-    print "Scrobbling %(songtitle)s by %(artist)s..." % song_info
-    CLIENT.submit({
-        'artist' : song_info['artist'],
-        'title'  : song_info['songtitle'],
-        'album'  : song_info['album'],
-        'length' : int(song_info['totalsec']),
-        'time'   : time.gmtime()
-    })
+    try:
+        print "Scrobbling %(songtitle)s by %(artist)s..." % song_info
+        CLIENT.submit({
+            'artist' : song_info['artist'],
+            'title'  : song_info['songtitle'],
+            'album'  : song_info['album'],
+            'length' : int(song_info['totalsec']),
+            'time'   : time.gmtime()
+        })
+    except KeyError:
+        # shouldn't happen, does anyway.
+        pass
 
 if __name__ == '__main__':
     mocevent.mainloop()

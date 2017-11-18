26967050cd55b49758f6f1d9f56a04c14311966a
Ken <kenjyco@gmail.com>
2017-11-18 12:34:52 -0600

    Bump to v0.4.3

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ with open('README.rst', 'r') as fp:
 
 setup(
     name='mocp',
-    version='0.4.2',
+    version='0.4.3',
     description='A Python library to control the MOC (music on console) audio player',
     long_description=long_description,
     author='Ken',
     author_email='kenjyco@gmail.com',
     license='MIT',
     url='https://github.com/kenjyco/mocp',
-    download_url='https://github.com/kenjyco/mocp/tarball/v0.4.2',
+    download_url='https://github.com/kenjyco/mocp/tarball/v0.4.3',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
